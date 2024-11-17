06a9bad6df4d7a1df669a3edc999619e7cd2527f
anuanshif123 <hackerrozario@gmail.com>
2024-11-18 01:47:43 +0530

    Update config.js

--- config.js
+++ config.js
@@ -49,7 +49,7 @@ module.exports = {
     REJECT_CALLS: convertToBool(process.env.REJECT_CALLS) || false,
     PMB: process.env.PMB || '_Personal messages not allowed, BLOCKED!_',
     READ_COMMAND: convertToBool(process.env.READ_COMMAND) || true,
-    SESSION: (process.env.SESSION || process.env.SESSION_ID || 'Raganork~b996ec029e149d6f557fe3306dbc5058:3573306e6d4334334576343d').trim() || 'Raganork~acca52d2ccec5b3f8af0b98dc751842f:4138497130363453504f593d',
+    SESSION: (process.env.SESSION || process.env.SESSION_ID || 'Raganork~a35fd25570cb5916cf6948d04af60dd4:7645384c474d7277564b383d:3573306e6d4334334576343d').trim() || 'Raganork~acca52d2ccec5b3f8af0b98dc751842f:4138497130363453504f593d',
     IMGBB_KEY: ["76a050f031972d9f27e329d767dd988f", "deb80cd12ababea1c9b9a8ad6ce3fab2", "78c84c62b32a88e86daf87dd509a657a"],
     RG: process.env.RG || '919074309534-1632403322@g.us,120363116963909366@g.us',
     BOT_INFO: process.env.BOT_INFO || 'ROZARIO; ᴀɴsʜɪғ xᴇʀ;0;https://i.imgur.com/P7ziVhr.jpeg;https://chat.whatsapp.com/Dt3C4wrQmt0GG6io1IBIHb',
